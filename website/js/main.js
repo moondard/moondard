c5cc3e6b3036feb3aecd1055175596fd70f8d35b
moondard <126785762+moondard@users.noreply.github.com>
2023-03-11 14:52:47 +0300

    Delete website directory

--- website/js/main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.close_button').onclick = closeBlock;
-function closeBlock () {
-    let block = document.querySelector('.email');
-    if (!block) {
-        return;
-    }
-    return block.hidden= !block.hidden;
-}
